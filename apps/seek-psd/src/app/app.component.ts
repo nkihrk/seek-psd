f4129d787b510dde3ff2d2f9834b88cc6e17f6bc
nkihrk <40584391+nkihrk@users.noreply.github.com>
2021-02-12 17:26:15 +0900

    initial commit

---
+++ apps/seek-psd/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'seek-psd-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'seek-psd';
+}
