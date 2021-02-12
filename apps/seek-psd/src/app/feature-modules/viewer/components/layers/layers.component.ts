f8a4ff8e3c0421db93e1ed294473dbc6d6cb8dbe
nkihrk <40584391+nkihrk@users.noreply.github.com>
2021-02-12 19:09:56 +0900

    [add/seek-psd] added components for the viewer-pages component

---
+++ apps/seek-psd/src/app/feature-modules/viewer/components/layers/layers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'seek-psd-layers',
+  templateUrl: './layers.component.html',
+  styleUrls: ['./layers.component.scss']
+})
+export class LayersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ apps/seek-psd/src/app/feature-modules/viewer/components/renderer/renderer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'seek-psd-renderer',
+  templateUrl: './renderer.component.html',
+  styleUrls: ['./renderer.component.scss']
+})
+export class RendererComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
