883e3ff24970062b925dd254c5ada587c6c74980
nkihrk <40584391+nkihrk@users.noreply.github.com>
2021-02-12 19:41:51 +0900

    [update/seek-psd] added a dark-mode component

---
+++ apps/seek-psd/src/app/feature-modules/viewer/components/dark-mode/dark-mode.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'seek-psd-dark-mode',
+  templateUrl: './dark-mode.component.html',
+  styleUrls: ['./dark-mode.component.scss']
+})
+export class DarkModeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- apps/seek-psd/src/app/feature-modules/viewer/viewer.module.ts
+++ apps/seek-psd/src/app/feature-modules/viewer/viewer.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { ViewerRoutingModule } from './viewer-routing.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { SharedModule } from '../../shared/shared.module';
+import { UiModule } from '@seek-psd/ui';
 
 import { ViewerPageComponent } from './pages/viewer-page/viewer-page.component';
 import { ToolsComponent } from './components/tools/tools.component';
 import { LayersComponent } from './components/layers/layers.component';
 import { RendererComponent } from './components/renderer/renderer.component';
 import { CanvasComponent } from './components/renderer/components/canvas/canvas.component';
+import { DarkModeComponent } from './components/dark-mode/dark-mode.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +19,14 @@ import { CanvasComponent } from './components/renderer/components/canvas/canvas.
     LayersComponent,
     RendererComponent,
     CanvasComponent,
+    DarkModeComponent,
   ],
-  imports: [CommonModule, ViewerRoutingModule, FlexLayoutModule, SharedModule],
+  imports: [
+    CommonModule,
+    ViewerRoutingModule,
+    FlexLayoutModule,
+    SharedModule,
+    UiModule,
+  ],
 })
 export class ViewerModule {}
