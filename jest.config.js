a37289d5447fa8d94c38b592b5af24bc54beaf0f
nkihrk <40584391+nkihrk@users.noreply.github.com>
2021-02-12 23:49:19 +0900

    [add/workspace] added engine2d

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,6 @@ module.exports = {
     '<rootDir>/apps/seek-psd',
     '<rootDir>/libs/ui',
     '<rootDir>/libs/utility-functions',
+    '<rootDir>/libs/engine2d',
   ],
 };
---
+++ libs/engine2d/README.md
@@ -0,0 +1,7 @@
+# engine2d
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test engine2d` to execute the unit tests.
