70c7022f997c1ee16f0c5b0fbf8b4fb0ac2b15f7
nkihrk <40584391+nkihrk@users.noreply.github.com>
2021-02-12 23:14:44 +0900

    [update/seek-psd] styled viewer views

--- apps/seek-psd/src/app/feature-modules/viewer/components/renderer/components/canvas/canvas.component.ts
+++ apps/seek-psd/src/app/feature-modules/viewer/components/renderer/components/canvas/canvas.component.ts
@@ -1,15 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  Output,
+  EventEmitter,
+} from '@angular/core';
+import { setPixelPerfect } from '@seek-psd/utility-functions';
+
+export interface Layers {
+  psdLayer: ElementRef<HTMLCanvasElement>;
+  uiLayer: ElementRef<HTMLCanvasElement>;
+}
 
 @Component({
   selector: 'seek-psd-canvas',
   templateUrl: './canvas.component.html',
-  styleUrls: ['./canvas.component.scss']
+  styleUrls: ['./canvas.component.scss'],
 })
 export class CanvasComponent implements OnInit {
+  @ViewChild('psdLayer', { static: true })
+  psdLayer: ElementRef<HTMLCanvasElement>;
+  @ViewChild('uiLayer', { static: true })
+  uiLayer: ElementRef<HTMLCanvasElement>;
 
-  constructor() { }
+  @Output() emitLayers = new EventEmitter<Layers>();
+
+  constructor() {}
 
   ngOnInit(): void {
+    // pass layers to the parent component
+    this._emitLayers();
+
+    // init canvas sizes with a appropriate pixel ratio
+    this._initCanvas();
   }
 
+  private _emitLayers(): void {
+    const layers: Layers = {
+      psdLayer: this.psdLayer,
+      uiLayer: this.uiLayer,
+    };
+
+    this.emitLayers.emit(layers);
+  }
+
+  private _initCanvas(): void {
+    setPixelPerfect(this.psdLayer.nativeElement, this._render);
+    setPixelPerfect(this.uiLayer.nativeElement, this._render);
+  }
+
+  private _render($canvas: HTMLCanvasElement): void {
+    //console.log($canvas);
+  }
 }
--- apps/seek-psd/src/app/feature-modules/viewer/components/renderer/renderer.component.ts
+++ apps/seek-psd/src/app/feature-modules/viewer/components/renderer/renderer.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Layers } from './components/canvas/canvas.component';
 
 @Component({
   selector: 'seek-psd-renderer',
   templateUrl: './renderer.component.html',
-  styleUrls: ['./renderer.component.scss']
+  styleUrls: ['./renderer.component.scss'],
 })
 export class RendererComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  getLayers($event: Layers): void {
+    console.log($event);
   }
-
 }
