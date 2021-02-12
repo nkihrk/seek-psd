c828b672b4173be9eb31f92dbb4ece43e5b5bb5c
nkihrk <40584391+nkihrk@users.noreply.github.com>
2021-02-12 19:10:38 +0900

    [update/seek-psd] moved a viewer-pages component to the pages folder

---
+++ apps/seek-psd/src/app/feature-modules/viewer/pages/viewer-page/viewer-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'seek-psd-viewer-page',
+  templateUrl: './viewer-page.component.html',
+  styleUrls: ['./viewer-page.component.scss']
+})
+export class ViewerPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
