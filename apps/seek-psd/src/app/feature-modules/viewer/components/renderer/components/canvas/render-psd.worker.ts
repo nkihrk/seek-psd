00a48158901957c93e2180fdc6c826e7c6c78ede
nkihrk <40584391+nkihrk@users.noreply.github.com>
2021-03-12 08:25:54 +0900

    [add/seek-psd] set configs and created files for web worker

---
+++ apps/seek-psd/src/app/feature-modules/viewer/components/renderer/components/canvas/render-psd.worker.ts
@@ -0,0 +1,15 @@
+import { DoWork, ObservableWorker } from 'observable-webworker';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@ObservableWorker()
+export class RenderPsd implements DoWork<string, string> {
+  public work(input$: Observable<string>): Observable<string> {
+    return input$.pipe(
+      map((message) => {
+        console.log(message); // outputs 'Hello from main thread'
+        return `Hello from webworker`;
+      })
+    );
+  }
+}
