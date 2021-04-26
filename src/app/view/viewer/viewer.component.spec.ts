cb86f3652cb33bfad73436614ff974f57a6b8f10
nkihrk <40584391+nkihrk@users.noreply.github.com>
2021-04-27 01:55:41 +0900

    [update] updated packages

--- src/app/view/viewer/viewer.component.spec.ts
+++ src/app/view/viewer/viewer.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ViewerComponent } from './viewer.component';
 
@@ -6,7 +6,7 @@ describe('ViewerComponent', () => {
 	let component: ViewerComponent;
 	let fixture: ComponentFixture<ViewerComponent>;
 
-	beforeEach(async(() => {
+	beforeEach(waitForAsync(() => {
 		TestBed.configureTestingModule({
 			declarations: [ViewerComponent]
 		}).compileComponents();
